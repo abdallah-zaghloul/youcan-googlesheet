fcedfe1099bc3e792455df463a4c6f224cb9665c
abdallah-zaghloul <3bdallahzaghloul@gmail.com>
2024-04-08 06:55:56 +0200

    view setting using axios

--- resources/components/Setting.vue
+++ resources/components/Setting.vue
@@ -1,10 +1,34 @@
 <script setup>
+import api from "@/ts/api.ts";
+import {ref, computed} from "vue";
 
+const {refData: setting} = await api.call({method: 'get', url: 'setting'});
 
 </script>
 
 <template>
     <div>
-       <h1>Setting</h1>
+        <div class="block-content" v-if="setting">
+            <form action="/setting" method="post">
+                <div class="is-grid grid-2">
+                    <div class="form-group">
+                        <label class="form-label">Google App Client ID</label>
+                        <input type="text"
+                               name="client_id"
+                               placeholder="Client ID"
+                               v-model="setting.client_id">
+                    </div>
+                    <div class="form-group">
+                        <label class="form-label">Google App Secret key</label>
+                        <input type="text"
+                               name="client_secret"
+                               placeholder="Client Secret Key"
+                               v-model="setting.client_secret">
+                    </div>
+
+                    <button type="submit" v-if="! setting.is_connected">Connect</button>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
